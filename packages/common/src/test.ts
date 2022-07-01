9bf21bef7ebe0c61e0d16df8b69a45ab8794bcb9
z9fr <z9fr@protonmail.com>
2022-07-02 00:01:36 +0530

    chore: monorepo changes to share common modules between server and web

---
+++ packages/common/src/test.ts
@@ -0,0 +1,3 @@
+export const testLog = () => {
+  console.log("value");
+};
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,4 +1,7 @@
 import express, { Request, Respose } from "express";
+import { sayHello } from "@surgeintern/common";
+import { testLog } from "@surgeintern/common/dist/test";
+
 const app = express();
 const port = 3001;
 
@@ -7,5 +10,7 @@ app.get("/data", (_req: Request, res: Respose) => {
 });
 
 app.listen(port, () => {
+  sayHello();
+  testLog();
   console.log(`Example app listening at http://localhost:${port}`);
 });
