292fa04372a358a1c57a1b4f75ff49bf7ea622e4
z9fr <z9fr@protonmail.com>
2022-07-06 07:35:30 +0530

    feat(web): save note by api call and small changes to the create note input box

--- packages/web/src/pages/dashboad.tsx
+++ packages/web/src/pages/dashboad.tsx
@@ -6,16 +6,54 @@ import {
   Paper,
   Text,
   Container,
-  UnstyledButton,
 } from "@mantine/core";
 import { useNotes } from "../query-hooks/notes";
 import { NoteCard } from "../components/Notes/cart";
 import { INote } from "../types/notes";
 import { useState } from "react";
+import { useForm } from "@mantine/form";
+import { displayNotification } from "../utils/showNotification";
+
+import httpClient from "../http-common";
+import { config } from "../config";
+
+interface INoteCreate {
+  title: string;
+  description: string;
+}
 
 export const Dashboard = () => {
   const notes = useNotes();
   const [createNote, setCreateNote] = useState(false);
+
+  const form = useForm({
+    initialValues: {
+      title: "",
+      description: "",
+    },
+  });
+
+  const submitForm = (values: INoteCreate, notes: any) => {
+    httpClient
+      .post(`notes/create`, JSON.stringify(values))
+      .then(function (response) {
+        displayNotification({
+          title: "Note created successfully",
+          color: "teal",
+          message: `note  ${values.title} created successfully`,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+        displayNotification({
+          title: error.response.data?.message,
+          message: error?.message,
+          color: "red",
+        });
+      });
+
+    notes.refetch();
+  };
 
   return (
     <>
@@ -23,25 +61,40 @@ export const Dashboard = () => {
         <Paper withBorder>
           {createNote ? (
             <>
-              <Input
-                variant="unstyled"
-                pl={10}
-                placeholder="Title"
-                size={"md"}
-              />
+              <form
+                onSubmit={form.onSubmit((values: INoteCreate) =>
+                  submitForm(values, notes)
+                )}
+              >
+                <Input
+                  variant="unstyled"
+                  pl={10}
+                  placeholder="Title"
+                  size={"md"}
+                  {...form.getInputProps("title")}
+                />
 
-              <Textarea
-                variant="unstyled"
-                pl={10}
-                placeholder="Take note..."
-                size="sm"
-                autosize
-                minRows={3}
-              />
+                <Textarea
+                  variant="unstyled"
+                  pl={10}
+                  placeholder="Take note..."
+                  size="sm"
+                  autosize
+                  minRows={3}
+                  {...form.getInputProps("description")}
+                />
 
-              <Button variant="default" compact uppercase ml={10} mb={10}>
-                Save
-              </Button>
+                <Button
+                  variant="default"
+                  compact
+                  uppercase
+                  ml={10}
+                  mb={10}
+                  type="submit"
+                >
+                  Save
+                </Button>
+              </form>
             </>
           ) : (
             <>
@@ -50,7 +103,8 @@ export const Dashboard = () => {
                 pl={10}
                 size={"md"}
                 placeholder="Take note..."
-                onClick={() => setCreateNote(true)}
+                {...form.getInputProps("title")}
+                onMouseEnter={() => setCreateNote(true)}
               />
             </>
           )}
@@ -62,6 +116,7 @@ export const Dashboard = () => {
         mt={30}
         spacing="lg"
         breakpoints={[{ maxWidth: "md", cols: 1 }]}
+        onMouseEnter={() => setCreateNote(false)}
       >
         {notes.isLoading && <> Loading </>}
         {notes.isError && (
